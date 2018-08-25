98e7e4e318d19ee959f89757f69d921d931fdc5f
Anastasiya Shtrakh <a4evo@yandex.ru>
2018-08-25 23:16:39 +0300

    feat: map works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
-import Map from './Map' 
-import Menu from './Menu' 
+import Map from './Map';
+import Menu from './Menu';
 
 import logo from './logo.svg';
 import './App.css';
 
+import places from './const/places.json';
+
 class App extends Component {
-  render() {
+
+	render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -16,7 +20,13 @@ class App extends Component {
         </header>
 		
         <Menu/>
-		<Map/>
+		<Map
+			googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBO37tWXY7797JXJmFXstlFy4J6rSMcu68&v=3.exp&libraries=geometry,drawing,places`}
+			loadingElement={<div style={{ height: `100%` }} />}
+			containerElement={<div id="map" role="application" />}
+  			mapElement={<div style={{ height: `100%` }} />}
+  			center={ places.center }
+		/>
 		
       </div>
     );
--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class Map extends Component {
-	render () {
+import styles from './const/map-styles'
+import places from './const/places'
+
+const Map = withScriptjs(withGoogleMap((props) =>{
+
+	const markers = []
+	const { center } = props
+
+	return (
 		
-		return (
-			<div id="map">Map</div>
-		)
-	}
+		<GoogleMap
+			defaultZoom={14}
+			center={ center.location }
+			defaultOptions={{ styles: styles,
+							mapTypeControl: false
+							}}
+
+			>
+			{markers}
+		</GoogleMap>
+
+
+    );
+}))
+
+Map.propTypes = {
+	center: PropTypes.object.isRequired
 }
 
 export default Map
