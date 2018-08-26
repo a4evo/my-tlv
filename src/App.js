1befe47a3f6f389532e1cae7cbd94567d3490dd2
Anastasiya Shtrakh <a4evo@yandex.ru>
2018-08-26 13:52:03 +0300

    feat: add menu list of places, onClick set active marker

--- src/App.js
+++ src/App.js
@@ -24,24 +24,30 @@ class App extends Component {
 	}
 
 	render() {
-
+		const { places, activeMarker } = this.state
     return (
       <div className="App">
+
         <header className="App-header">
           <img src={ logo } className="App-logo" alt="logo" />
           <h1 className="App-title">My Tel Aviv</h1>
         </header>
 		
-        <Menu/>
+        <Menu
+			places={ places }
+			activeMarker={ activeMarker }
+			onLiClick={ ( place ) => this.setActiveMarker( place ) }
+		/>
+
 		<Map
 			googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBO37tWXY7797JXJmFXstlFy4J6rSMcu68&v=3.exp&libraries=geometry,drawing,places`}
 			loadingElement={<div style={{ height: `100%` }} />}
 			containerElement={<div id="map" role="application" />}
   			mapElement={<div style={{ height: `100%` }} />}
   			center={ placesJSON.center }
-  			places={ this.state.places }
-  			activeMarker={ this.state.activeMarker }
-  			onMarkerClick={ ( place ) => this.setActiveMarker(place)}
+  			places={ places }
+  			activeMarker={ activeMarker }
+  			onMarkerClick={ ( place ) => this.setActiveMarker( place )}
 		/>
 		
       </div>
--- src/Menu.js
+++ src/Menu.js
@@ -3,10 +3,34 @@ import PropTypes from 'prop-types'
 
 class Menu extends Component {
 	
+	static propTypes = {
+		places: PropTypes.array.isRequired,
+		activeMarker: PropTypes.object.isRequired,
+		onLiClick: PropTypes.func.isRequired
+	}
+
 	render () {
-		
+		const { places, activeMarker } = this.props
 		return (
-			<div id="menu">Menu</div>
+			<div id="menu">Menu
+				<input type="text" placeholder="Type to filter"/>
+				{ places.length > 0 ? (
+					<ul>
+						{places.map( place => (
+							<li key={place.place_id}>
+								<button onClick={ () => this.props.onLiClick(place)}>
+									{ place.title }
+								</button>
+							</li>
+						))}
+					</ul>
+				) : (
+					<div>Nothing found</div>
+				)
+
+				}
+
+			</div>
 		)
 	}
 }
